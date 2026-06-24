b7b3ee0e42f51eae5f8ef67d2ea4da50de510662
Rodolfo037 <IsengardxD@gmail.com>
2026-06-23 21:26:44 -0500

    Ejercicio Cuadrado(n)

---
+++ ejercicio2_RMP.py
@@ -0,0 +1,8 @@
+#Escribe una función cuadrado(n) que reciba un entero y
+#  retorne su cuadrado. Muestra el resultado.
+
+def cuadrado(n):
+    return n ** 2
+
+cuad = cuadrado(5)
+print (f"{5} al cuadrado es igual a : {cuad}")
