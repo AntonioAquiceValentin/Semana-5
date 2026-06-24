333cfee24cb15ffde54d63a2265372e8c8fc912a
Rodolfo037 <IsengardxD@gmail.com>
2026-06-23 21:24:39 -0500

    Ejercicio Saludar(nombre)

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+#Crea una función saludar(nombre) que reciba un 
+# nombre como cadena e imprima un saludo personalizado.
+
+def saludar(nombre):
+    return (f"Hola {nombre}")
+
+nom = saludar("Rodolfo")
+print(nom)
